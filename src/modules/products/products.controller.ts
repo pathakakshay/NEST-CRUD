06b523e75a470f176fba896b8da1b1007ecede12
pathakakshay <akshay@techtic.agency>
2021-08-03 19:05:26 +0530

    fix error

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -8,12 +8,16 @@ import {
   Delete,
   Patch,
 } from '@nestjs/common';
+import { ApiOkResponse, ApiBadRequestResponse } from '@nestjs/swagger';
+
 import { productDTO } from 'src/shared/dto/product.dto';
 import { ProductService } from 'src/shared/services';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly prodctService: ProductService) {}
   @Post()
+  @ApiOkResponse({ description: 'Successfully authenticated' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   async addProduct(@Body() body: productDTO): Promise<any> {
     try {
       const generatedId = await this.prodctService.insertProduct(
@@ -28,6 +32,8 @@ export class ProductsController {
   }
 
   @Get()
+  @ApiOkResponse({ description: 'Successfully authenticated' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   async getAllProducts(): Promise<any> {
     try {
       const products = await this.prodctService.getAllProduct();
@@ -38,29 +44,48 @@ export class ProductsController {
   }
 
   @Get('/:id')
+  @ApiOkResponse({ description: 'Successfully authenticated' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   async getProduct(@Param('id') prodId: number): Promise<any> {
-    return await this.prodctService.getSingleProduct(prodId);
+    try {
+      const product = await this.prodctService.getSingleProduct(prodId);
+      return product;
+    } catch (error: any) {
+      throw new UnprocessableEntityException(error);
+    }
   }
 
   @Patch('/:id')
+  @ApiOkResponse({ description: 'Successfully authenticated' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   async updateProduct(
     @Param('id') prodId: string,
     @Body('title') prodTitle: string,
     @Body('description') prodDescription: string,
     @Body('price') prodPrice: number
   ): Promise<any> {
-    this.prodctService.updateProduct(
-      prodId,
-      prodTitle,
-      prodDescription,
-      prodPrice
-    );
-    return null;
+    try {
+      await this.prodctService.updateProduct(
+        prodId,
+        prodTitle,
+        prodDescription,
+        prodPrice
+      );
+      return null;
+    } catch (error: any) {
+      throw new UnprocessableEntityException(error);
+    }
   }
 
   @Delete('/:id')
+  @ApiOkResponse({ description: 'Successfully authenticated' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
   async removeProduct(@Param('id') prodId: number): Promise<any> {
-    this.prodctService.deleteProduct(prodId);
-    return null;
+    try {
+      await this.prodctService.deleteProduct(prodId);
+      return null;
+    } catch (error: any) {
+      throw new UnprocessableEntityException(error);
+    }
   }
 }
